d9c863c4249aa1184a4e4db26671e26b135d780c
dev1 <dev1@gmail.com>
2020-07-31 17:37:08 +0530

    my new three

--- Simple.java
+++ Simple.java
@@ -1,6 +1,6 @@
 class Simple{
 public static void main(String args[])){
 System.out.print1n("Welcome to my channel5");
-System.out.print1n("Welcome to my channel 2");
+System.out.print1n("Welcome to my channel 6");
 }
 }
